c4781e71b970a189771f548e9b4f99eabedc46ee
ธนบดี ภูชมศรี <6801073@kkfnets.kkf>
2026-03-12 17:55:55 +0700

    add images Q

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
     API_KEY: 'AIzaSyDdoE28Fk8NDq_vmj6k17xOWzQp6mVjbiI',
     SHEET_ID: '1qEHMS50qs95rAwcGzFZxyx3uJMqsjvgSKIqVjL3fSBg',
-    RANGE: 'Sheet1!A2:N'
+    RANGE: 'Sheet1!A2:Q'
 };
